a971086f8ab4f7bb72161e4c16fceeab671ba989
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-22 01:22:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,7 @@ def num_points_scored(players_name)
     team.each do |attribute, data|
       if attribute == :players
         data.each do |player|
-          binding.pry
+      
           if player[:player_name] == players_name
             #returnplayer[:points] 
             result = player[:points]
