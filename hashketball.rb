ec56e3f47eb89e8d929ad8c3c14312ef68409e82
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-22 02:23:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,13 +188,13 @@ def player_stats(players_name)
       if player[:players_name] = players_name
         new_hash = player.delete_if do |k, v|
           k == :player_name
+            end
           end
         end
       end
     end
   end
-end
-new_hash
+  new_hash
 end
 
 
