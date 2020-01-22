e31c564f35b92ac7d375effdfa05ad7536d5dc26
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-22 02:12:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
   game_hash.map do |place, team|
     if team[:team_name] == team_name
       team.each do |attribute, data|
-        if attribute == :players
+        if attributes == :players
           data[:numbers]
       end
     end
